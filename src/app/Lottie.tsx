3b9ea8f2f177ebc201552c825628edbda08e8059
riswan ardiansah <riswanardiansah@gmail.com>
2025-01-03 23:37:58 +0700

    add animation

---
+++ src/app/Lottie.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import Lottie from 'react-lottie';
+import * as animationData from '../../public/stars.json'
+import { useEffect, useState } from 'react';
+
+type LottieCompnentProps = {
+  className?: string
+  delay: number
+}
+
+const defaultOptions = {
+  loop: true,
+  autoplay: false, 
+  animationData: animationData,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice'
+  }
+};
+
+const LottieComponent = ({className, delay}: LottieCompnentProps) => {
+  const [play, setPlay] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setPlay(true)
+    }, delay)
+  }, [delay])
+  return (
+    <div className={'absolute ' + className}>
+      <Lottie 
+        options={defaultOptions}
+        width={100}
+        height={50}
+        isStopped={!play}
+      />
+    </div>
+  )
+}
+
+export default LottieComponent
