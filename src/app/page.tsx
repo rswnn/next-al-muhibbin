fcf52a927b4dbcbbc17847ce9ddba632ec016b0a
riswan ardiansah <riswanardiansah@gmail.com>
2025-01-04 11:22:08 +0700

    change component directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-import {Suspense} from "react";
+import { Suspense } from "react";
 import Image from "next/image";
 import { Montserrat } from 'next/font/google'
-import LottieComponent from './Lottie';
+import LottieComponent from '../components/Lottie';
 
 const roboto = Montserrat({
   weight: '400',
@@ -16,16 +16,16 @@ export default function Home() {
           <div className="sm:container sm:mx-auto relative bg-gradient-to-b from-[#302c6c] to-[#9d2d86] px-5 pb-16 pt-10 overflow-hidden">
             <div className='flex'>
               <Suspense>
-              <LottieComponent delay={1000} className='right-0'/>
-              <LottieComponent delay={2000}/>
+                <LottieComponent delay={1000} className='right-0'/>
+                <LottieComponent delay={2000}/>
               </Suspense>
             </div>
             <img src="./lamp.png" alt="lamp" className='w-20 absolute top-[-50px] left-5' />
             <Image alt='img-broken' src="/master-almuhibbin.png" className='mx-auto block mb-14' width={110} height={110} />
             <div className='flex'>
               <Suspense>
-              <LottieComponent delay={3000}/>
-              <LottieComponent delay={4000} className='right-0'/>
+                <LottieComponent delay={3000}/>
+                <LottieComponent delay={4000} className='right-0'/>
               </Suspense>
             </div>
             <img src="./lamp.png" alt="lamp" className='w-20 absolute top-[-50px] right-5' />
@@ -36,9 +36,9 @@ export default function Home() {
             <p className='text-xs'>Tanggal: 11 Januari 2025</p>
             <div className='flex'>
               <Suspense>
-              <LottieComponent delay={5000}/>
-              <LottieComponent delay={6000} className='right-0'/>
-                </Suspense>
+                <LottieComponent delay={5000}/>
+                <LottieComponent delay={6000} className='right-0'/>
+              </Suspense>
             </div>
             <p className='text-xs'>Waktu: 19.00 WIB</p>
             <p className='text-xs'>Tempat : Masjid Abu Bakar Ash-Shiddiq Pala Bali RW 6 Boponter Depok</p>
@@ -47,8 +47,8 @@ export default function Home() {
             <img src="./mosque.png" alt="mosque" className='w-screen absolute bottom-0 left-0 opacity-20' />
             <div className='flex'>
               <Suspense>
-              <LottieComponent delay={7000}/>
-              <LottieComponent delay={8000} className='right-0'/>
+                <LottieComponent delay={7000}/>
+                <LottieComponent delay={8000} className='right-0'/>
                 </Suspense>
             </div>
             {/* <div className="w-screen h-screen md:w-[719px] md:h-[984px]">
