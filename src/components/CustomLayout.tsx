595fba06cffd1b8d79c87e8765c53848de54f3c3
riswan ardiansah <riswanardiansah@gmail.com>
2025-01-04 11:38:12 +0700

    change import

--- src/components/CustomLayout.tsx
+++ src/components/CustomLayout.tsx
@@ -3,7 +3,7 @@
 /* eslint-disable @next/next/no-img-element */
 import Image from "next/image";
 import { Montserrat } from 'next/font/google'
-import LottieComponent from '../components/Lottie';
+import LottieComponent from './Lottie';
 
 const roboto = Montserrat({
   weight: '400',
