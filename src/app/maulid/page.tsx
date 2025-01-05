87d293bbad9f29199e24cbce68ecc92e486ec457
riswan ardiansah <riswanardiansah@gmail.com>
2025-01-05 17:15:29 +0700

    change meta data #4

--- src/app/maulid/page.tsx
+++ src/app/maulid/page.tsx
@@ -2,7 +2,12 @@ import { Suspense } from 'react';
 
 import 'animate.css';
 import Content from './content';
+import type { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "Al-muhibbin",
+  description: "Undangan Maulid Nabi Muhammad SAW dan Milad Majelis Ta'lim Al-Muhibbin",
+};
 
 const MaulidPage = () => {
   return (
